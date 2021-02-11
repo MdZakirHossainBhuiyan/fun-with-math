b52323d810e9cbba573c20982b65d4f32b66bd36
Md Zakir Hossain Bhuiyan <zakirhossaintuhin@gamil.com>
2021-02-11 14:05:03 +0600

    fun card 1

---
+++ app.js
@@ -0,0 +1,63 @@
+document.getElementById('start-btn').addEventListener('click', () => {
+    const startBtn = document.getElementById('start-btn');
+    startBtn.style.display = "none";
+    const stepBtn1 = document.getElementById('first-card');
+    stepBtn1.style.display = "block";
+});
+
+document.getElementById('step-btn1').addEventListener('click', () => {
+    const startBtn = document.getElementById('first-card');
+    startBtn.style.display = "none";
+    const stepBtn1 = document.getElementById('second-card');
+    stepBtn1.style.display = "block";
+});
+
+document.getElementById('step-btn2').addEventListener('click', () => {
+    const startBtn = document.getElementById('second-card');
+    startBtn.style.display = "none";
+    const stepBtn1 = document.getElementById('third-card');
+    stepBtn1.style.display = "block";
+});
+
+document.getElementById('step-btn3').addEventListener('click', () => {
+    const startBtn = document.getElementById('third-card');
+    startBtn.style.display = "none";
+    const stepBtn1 = document.getElementById('fourth-card');
+    stepBtn1.style.display = "block";
+});
+
+document.getElementById('step-btn4').addEventListener('click', () => {
+    const startBtn = document.getElementById('fourth-card');
+    startBtn.style.display = "none";
+    const stepBtn1 = document.getElementById('fifth-card');
+    stepBtn1.style.display = "block";
+});
+
+document.getElementById('step-btn5').addEventListener('click', () => {
+    const startBtn = document.getElementById('fifth-card');
+    startBtn.style.display = "none";
+    const stepBtn1 = document.getElementById('sixth-card');
+    stepBtn1.style.display = "block";
+});
+
+document.getElementById('step-btn6').addEventListener('click', () => {
+    const startBtn = document.getElementById('sixth-card');
+    startBtn.style.display = "none";
+    const stepBtn1 = document.getElementById('seventh-card');
+    stepBtn1.style.display = "block";
+});
+
+document.getElementById('step-btn7').addEventListener('click', () => {
+    const startBtn = document.getElementById('seventh-card');
+    startBtn.style.display = "none";
+    const stepBtn1 = document.getElementById('eight-card');
+    stepBtn1.style.display = "block";
+});
+
+document.getElementById('step-btn8').addEventListener('click', () => {
+    const startBtn = document.getElementById('eight-card');
+    startBtn.style.display = "none";
+    const stepBtn1 = document.getElementById('start-btn');
+    stepBtn1.style.display = "block";
+});
+
